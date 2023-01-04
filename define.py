af6544a1ffbfece87b705e13c40cbaef4d4c3b84
valentin <alzobavalentin@mail.ru>
2023-01-04 21:42:15 +0300

    neew

--- define.py
+++ define.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 
 import random
+import time
+class R:
+    def main_func():
+        a = []
+        def inner_func():
+            for i in range(5):
+                a.append(random.randint(12,232))
+                time.sleep(0.4)
+                b = []
+                for j in range(4): 
+                    b.append(random.randint(1,12))
+                    b.extend(a)
+                    print(sorted(b, key=None, reverse=True))
+                    time.sleep(0.4)
+        return inner_func()
+    main_func()
 
-for i in range(1,8):
-    for j in range(i,8):
-        print(1,'\t ', 0,'\v',2)
+if __name__=='__main__':
+    R
+            
+
