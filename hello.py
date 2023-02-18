79b4a12cb5e9e6d95e0c1b4e161d35cd9e6e4699
walentik39 <alzobavalentin@mail.ru>
2023-02-18 21:44:32 +0300

    new_hello

--- hello.py
+++ hello.py
@@ -4,9 +4,9 @@ import time
 
 s = (x ** 2 for x in range(int(input())))
 for i in s:
+    time.sleep(2)        
     if i%2==0:
         print(i)
     else:
         print(i ** 0.5)
 
-time.sleep(2)        
