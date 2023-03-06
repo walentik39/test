7c55a487a679383bb3c0cee75aac447c86f35491
walentik39 <alzobavalentin@mail.ru>
2023-03-06 20:44:03 +0300

    edit hello

--- hello.py
+++ hello.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 
 import time
+import random
 
-s = (x ** 2 for x in range(int(input())))
-for i in s:
-    if i%2==0:
-        print(i)
-    else:
-        print(i ** 0.5)
+class A:
+    def f():
+        d = {}
+        s = []
+        i = 1
+        while i < 6:
+            s.append(random.randint(1,99))
+            s = sorted(s, key=None, reverse=True)
+            d[i] = s
+            time.sleep(0.2)
+            i += 1
+            print(d)
+    f()
 
-time.sleep(2)        
+if __name__=='__main__':
+    A
