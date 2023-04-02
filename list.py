a5968e736e1d48aceb2844cd0be68c9f3981b78f
walentik39 <alzobavalentin@mail.ru>
2023-04-02 21:34:13 +0300

    edit list

--- list.py
+++ list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import random
+
 empty_list = []
-freands = ['Макс','123','Leo','John']
+freands = ['Макс','123','Leo','John','Вера','Иван','Json']
 print(freands[:2])
 freands.append('Ron')
 print(len(freands))
-empty_list.append(freands)
+empty_list.append(random.choice(freands))
 print(empty_list)
