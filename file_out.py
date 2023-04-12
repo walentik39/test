3555716bfa089c4b68dceb9ac621eb86313042a6
walentik39 <alzobavalentin@mail.ru>
2023-04-12 22:48:39 +0300

    edit file_out

--- file_out.py
+++ file_out.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 import sys
-try:
-    with open('test2.odt') as f:
-        x = f.readline()
-        for i in x:
-            print(i, end='')
-except IOError as e:
-    print("{}\nОшибка при открытии {}. Завершение программы.". format(e, file), file=sys.stderr)
-    sys.exit(1)
+def load():
+    """ Открыть текстовый файл и вернуть список цепочек символов
+    в нижнем регистре."""
+    try:
+        with open('test.odt') as in_file:
+            loaded_txt = in_file.read().strip() .split('\n')
+            loaded_txt = [x.lower() for x in loaded_txt]
+            print(loaded_txt)
+    except IOError as e:
+            print("{}\nОшибка при открытии {}. Завершение программы.". format(e, file), file=sys.stderr)
+            sys.exit(1)
+load()
