469c4e885bbfc47668e18e5799ee8aaeef1a5091
walentik39 <alzobavalentin@mail.ru>
2023-04-17 23:11:53 +0300

    edit list

--- list.py
+++ list.py
@@ -4,8 +4,8 @@ import random
 
 empty_list = []
 freands = ['Макс','123','Leo','John','Вера','Иван','Json']
-print(freands[:2])
-freands.append('Ron')
-print(len(freands))
-empty_list.append(random.choice(freands))
-print(empty_list)
+i = 0
+while i < len(freands) + 1:
+    print(sorted(empty_list, key=None, reverse=False))
+    empty_list.append(random.choice(freands))
+    i += 1
