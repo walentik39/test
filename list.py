8082356a6bb079165a0e1c1f92823eb9f9c6856d
walentik39 <alzobavalentin@mail.ru>
2023-06-27 13:57:02 +0300

    edit list

--- list.py
+++ list.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
 import random
+import subprocess
 
-empty_list = []
-freands = ['Макс','123','Leo','John','Вера','Иван','Json']
-i = 0
-while i < len(freands) + 1:
-    print(sorted(empty_list, key=None, reverse=False))
-    empty_list.append(random.choice(freands))
-    i += 1
+def read_list(name:str) -> list:
+    empty_list = []
+
+    def names():
+        freands = ['Макс','Петро','Leo','John','Вера','Иван','Json']
+        for i in range(len(freands)):
+            empty_list.append(random.choice(freands))
+            with open('list.odt','w') as file:
+                file.write(str(empty_list))
+    return names()
+
+def read_file():
+    with open('list.odt') as file:
+        result = file.read()
+        for i in result:
+            print(i.rstrip(),end=' ')
+
+
+if __name__=='__main__':
+    read_list('Вася')
+    read_file()
