62c1ddfaace3ab4474e24760988adf8b935f179e
walentik39 <alzobavalentin@mail.ru>
2023-08-23 23:07:27 +0300

    edit_shifr

--- shifr.py
+++ shifr.py
@@ -4,7 +4,7 @@ import random
 
 def fun():
     s = input("Введите строку: ")
-    a = s.split()
+    a = s.split() + list(str(random.randint(0,9)))
     print(''.join(a))
 if __name__=='__main__':
     fun()
