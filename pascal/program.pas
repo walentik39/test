fca5998f704ca1855026b4c5d73e1c4dd7239eed
walentik39 <alzobavalentin@mail.ru>
2023-11-06 20:35:39 +0300

    mkdir_pascal

---
+++ pascal/program.pas
@@ -0,0 +1,14 @@
+var
+	a,b:integer;
+	c:double;
+	d:real;
+begin
+	write('inter number: ');
+	read(a);
+	write('inter two number: ');
+	read(b);
+	c:=(a+b)/2;
+	writeln(c);
+	d:=sqrt(a*a + b*b);
+	write(d);
+end.
