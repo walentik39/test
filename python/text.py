f2a1dd14e2ffdf4c7e7950a766f30bedbc3c7956
walentik39 <alzobavalentin@mail.ru>
2023-11-10 21:07:37 +0300

    edit_text

--- python/text.py
+++ python/text.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
 class My:
+
     def fun():
-        os.system('host -A mail.ru > test.odt')
-        f = open('test.odt','r+')
-        os.system('cat test.odt')
-        print(f)
-        f.close()
+        result = subprocess.run(['host','mail.ru'],stderr=subprocess.PIPE,
+                                stdout=subprocess.PIPE)
+        with open('test.odt','w') as file:
+            file.write(str(result))
+
+    def read_file():
+        with open('test.odt','r') as file:
+            for i in file:
+                print(''.join(i))
+
 
 if __name__=='__main__':
-    fun=My.fun()
+    My.fun()
+    My.read_file()
