49f19efb0e3356c15ea51722506e8434e9b19b23
walentik39 <alzobavalentin@mail.ru>
2024-01-03 22:45:35 +0300

    edit def_2

--- python/def_2.py
+++ python/def_2.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 
 import random
-class A:
-    def main():
-        def two_main():
-            x = []
-            y = []
-            i = 0
-            while i < 7:
-                x.append(random.randint(1,20))
-                y.append(random.choice(x))
-                print(y[::-1])
-                i += 1
-        return two_main()
-    main()
+def func():
+    x = []
+    y = []
+    i = 0
+    while i < 7:
+        x.append(random.randint(1,20))
+        y.append(random.choice(x))
+        print(y[::-1])
+        i += 1
 
 if __name__=='__main__':
-    A
-
+    func()
