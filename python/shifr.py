0d48db5572ab6ca4c1ebd918489329fd4d4500d6
walentik39 <alzobavalentin@mail.ru>
2024-02-28 22:26:34 +0300

    edit shifr

--- python/shifr.py
+++ python/shifr.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 import random
-
+import subprocess
+import os
 def fun():
     s = input("Введите строку: ")
+    with open('test.odt','w') as f:
+        f.write(s)
+    f.close()    
     a = []
     for i in reversed(range(1,len(s)+1)):
         a +=[i]
