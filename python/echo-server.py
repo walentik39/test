2d744983c21ac31d55e96085093e483d8eb7f81d
walentik39 <alzobavalentin@mail.ru>
2024-03-12 15:49:56 +0300

    add file

---
+++ python/echo-server.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from socket import *
+myHost = ''
+myPort = 50007
+
+sockobj = socket(AF_INET, SOCK_STREAM)
+sockobj.bind((myHost, myPort))
+sockobj.listen(5)
+
+while True:
+    connection, address =sockobj.accept()
+    print('Server connect', address)
+
+    while True:
+        data = connection.recv(1024)
+        if not data:
+            break
+        connection.send(b'Echo=>', + data)
+    connection.close()    
