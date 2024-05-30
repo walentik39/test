b7c854feb6ed09e8d19a2e46b99d9dc574c36b79
walentik39 <alzobavalentin@mail.ru>
2024-05-30 21:09:56 +0300

    edit three

--- python/three.py
+++ python/three.py
@@ -6,17 +6,17 @@ import sys
 import random
 
 def one():
-    return subprocess.run(['ip','a'],stdout=subprocess.PIPE)
+    result = subprocess.run(['ip','a'],stdout=subprocess.PIPE,
+                            stderr=subprocess.DEVNULL,encoding='utf-8')
+    return result.stdout
 
 def rand_fun():
     return random.randint(0,9)
 
-def write_file(name:str):
-    with open('prim.md','w') as file:
-        file.write(name)
-
 if __name__=='__main__':
-    for i in range(rand_fun()):
-        write_file(str(i))
-        print(i)
-
+    with open('prim.md','w') as file:
+        file.write(str(one()))
+        file.write(str(rand_fun()))
+    with open('prim.md','r') as f:
+        res = f.read()
+        print(res)
