43bb399640751e444c88d03cffc32f7408e4c698
walentik39 <alzobavalentin@mail.ru>
2024-08-12 22:24:58 +0300

    add file

---
+++ python/rand_world.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+import random
+
+def fun():
+    last = ('Иван','Фёдр','Пилип','Андрей','"Соловей-разбойник"','Валентин','Марина','Анна','Вера','Юля','Мария')
+    first = ('строитель','маляр','электрик','врач','тракторист','водитель','кранавщик','столяр','слесарь')
+    while True:
+        lastName = random.choice(last)
+        firstName = random.choice(first)
+        print("\n\n")
+        print("{} {}".format(lastName, firstName),file=sys.stderr)
+        print("\n\n")
+        try_again = input("\n\nПопробуйте ещё? (Нажмите Enter либо n, чтобы выйти)\n")
+        if try_again.lower() == "n":
+            break
+
+if __name__=='__main__':
+    fun()
