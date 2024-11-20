0e6c454944cef816599740de22240bbb45728e88
walentik39 <alzobavalentin@mail.ru>
2024-11-20 17:54:20 +0300

    add file

--- python/shifr.py
+++ python/shifr.py
@@ -3,15 +3,17 @@
 import random
 import subprocess
 import os
-def fun():
-    s = input("Введите строку: ")
-    with open('test.odt','w') as f:
-        f.write(s)
-    f.close()    
-    a = []
-    for i in reversed(range(1,len(s)+1)):
-        a +=[i]
-        print(''.join(str(a)))
+class F:
+    def fun(self):
+        s = input("Введите строку: ")
+        with open('test.odt','w') as f:
+            f.write(s)
+            f.close()    
+        a = []
+        for i in reversed(range(1,len(s)+1)):
+            a +=[i]
+            print(''.join(str(a)))
 
 if __name__=='__main__':
-    fun()
+    m = F()
+    m.fun()
