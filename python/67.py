453a07b14e02f4ee122be6cef50cb1a9e32ff884
walentik39 <alzobavalentin@mail.ru>
2024-11-25 20:24:26 +0300

    add files

---
+++ python/67.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+string = input("Введите строку: ")
+print(string.upper())
---
+++ python/68.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+string = input()
+my_string = string.upper()
+print(my_string.lower())
