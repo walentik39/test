c68dddbbdd28845c233d47805aca8351ef8ab28d
walentik39 <alzobavalentin@mail.ru>
2024-11-27 14:34:27 +0300

    using mkdir and handlink already create dirs

--- shell/script.sh
+++ shell/script.sh
@@ -6,6 +6,12 @@ echo "Hello World"
 cur=1
 while [[ "$cur" -le 10 ]];
 do
-	echo "$cur"
+	if [[ -e $cur ]]
+	then
+		echo File $cur already exists
+		cur=$(( cur + 1 ))
+	        continue
+	fi	
+	mkdir "$cur"
 	cur=$(( cur + 1 ))
 done
