afbaad9b4c4ee7e518f15fde91581ead6ddadc3b
walentik39 <alzobavalentin@mail.ru>
2024-12-11 20:10:17 +0300

    add files

--- python/t_subprocess.py
+++ python/t_subprocess.py
@@ -2,21 +2,25 @@
 
 import subprocess
 import os
+import random
 
-def ping():
-    ip_list = ['8.8.4.4','https://www.freebsd.org','test.me','0.0.0.0']
-    for ip in ip_list:
-        result = subprocess.run(
-                ['ping','-c','1','-n', ip],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                encoding="utf-8"
-                )
-        return result.stdout
+class Sub:
+    def ping(self):
+        ip_list = ['8.8.4.4','https://www.freebsd.org','test.me','0.0.0.0']
+        random.shuffle(ip_list)
+        for ip in ip_list:
+            result = subprocess.run(
+                    ['ping','-c','1','-n', ip],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    encoding="utf-8"
+                    )
+            return result.stdout
 
-def write():
-    with open('test.md','w') as f:
-        f.write(str(ping()))
+    def write(self):
+        with open('test.md','w') as f:
+            f.write(str(self.ping()))
 
 if __name__=='__main__':
-    write()
+    s = Sub()
+    s.write()
