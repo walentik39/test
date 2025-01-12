5493c7472eb5744fe696790deabc658b480344ee
walentik39 <alzobavalentin@mail.ru>
2025-01-12 21:08:34 +0300

    edit reversing.py

--- python/reversing.py
+++ python/reversing.py
@@ -2,27 +2,31 @@
 
 import sys
 import subprocess
+class My:
+    def hook_file(self):
+        print('Всё работает!')
+        sys.exit()
 
-def hook_file():
-    print('Всё работает!')
-    sys.exit()
-
-def wait():
-    result = subprocess.run(['lsof','-i'],stdout=subprocess.PIPE,
+    def wait(self):
+        result = subprocess.run(['lsof','-i'],stdout=subprocess.PIPE,
                             stderr =subprocess.DEVNULL,encoding='utf-8')
-    with open('test.odt','w') as f:
-        f.write(str(result))
-    print('Ждём...')
+        with open('test.odt','w') as f:
+            f.write(str(result))
+        print('Ждём...')
 
-print('Взять червя')
-print('наживить червя')
-print('закинуть наживку')
+        print('Взять червя')
+        print('наживить червя')
+        print('закинуть наживку')
 
-while True:
-    response = input('Поплавок дёрнулся? ')
-    if response == 'да':
-        ia_moving = True
-        print('клюнуло ')
-        hook_file()
-    else:
-        wait()
+        while True:
+            response = input('Поплавок дёрнулся? ')
+            if response == 'да':
+                ia_moving = True
+                print('клюнуло ')
+                self.hook_file()
+            else:
+                self.wait()
+if __name__=='__main__':
+    m = My()
+    m.wait()
+    m.hook_file()
