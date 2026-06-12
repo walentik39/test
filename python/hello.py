f44eb190afce72f20cd03651d1a6a32d59433de0
walentik39 <alzobavalentin@mail.ru>
2026-06-12 20:16:06 +0300

    edit hello.py

--- python/hello.py
+++ python/hello.py
@@ -5,16 +5,16 @@ import random
 import os
 
 class A:
-    def main():
+    def main(self):
         os.system('lsof -i > test.odt')
-        def two_main():
+        def two_main(self):
             with open('test.odt','r+') as myfile:
                 s = myfile.read()
                 print(s)
             os.system('> test.odt')    
-        return two_main()
-    main()
+        return two_main(self)
 
 if __name__=='__main__':
-    A
+    a = A()
+    a.main()
 
